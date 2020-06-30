c21630becb9134e2a976749fef777beb85658581
shayne243 <marques0@purdue.edu>
2020-06-30 16:29:47 +0400

    Added menu and all pages

--- src/home.js
+++ src/home.js
@@ -1,28 +1,6 @@
 const mainPage = () => {
+    document.querySelector("body").setAttribute("style", "background-color: #ffebcd")
     const mainDiv = document.querySelector("#content"); 
-    const mainContent = () => {
-        const contentDiv = document.querySelector("#tab-content");
-        const mainHeading = document.createElement("h1");
-        mainHeading.setAttribute("id", "restaurant-name");
-        mainHeading.setAttribute("style", "text-align: center; font-family: Georgia, 'Times New Roman', Times, serif")
-        mainHeading.innerHTML = "Welcome to Cactus Restaurant and Grill";
-        const descriptionHeading = document.createElement("h2");
-        descriptionHeading.setAttribute("id", "description-heading");
-        descriptionHeading.setAttribute("style", "text-align: center; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif");
-        descriptionHeading.innerHTML = "Our world class flavors combined with our system of an unlimited buffet results in a premium dining experience for our guests."
-        const foodImage = document.createElement("img");
-        foodImage.setAttribute("id", "food");
-        foodImage.setAttribute("src", "../images/food.jpg");
-        foodImage.setAttribute("style", "width: 600px; height: 337.5px; display: block; margin: 10 auto;");
-        const setupImage = document.createElement("img");
-        setupImage.setAttribute("id", "setup");
-        setupImage.setAttribute("src", "../images/setup.jpg");
-        setupImage.setAttribute("style", "width: 600px; height: 337.5px; display: block; margin: auto;");
-        contentDiv.appendChild(mainHeading);
-        contentDiv.appendChild(descriptionHeading);
-        contentDiv.appendChild(foodImage);
-        contentDiv.appendChild(setupImage);
-    }
     const navBar = () => {
         const navDiv = document.createElement("div");
         navDiv.setAttribute("id", "nav-bar");
@@ -30,6 +8,7 @@ const mainPage = () => {
     }
     const navContent = () => {
         const navDiv = document.querySelector("#nav-bar");
+        navDiv.setAttribute("style", "cursor: pointer; border: solid; display: flex; align-items: center; justify-content: center;")
         const initTab = document.createElement("div");
         initTab.classList.add("nav-div");
         initTab.setAttribute("style", "text-align: center; font-family: Georgia, 'Times New Roman', Times, serif");
@@ -45,6 +24,9 @@ const mainPage = () => {
         secondTab.setAttribute("style", "text-align: center; font-family: Georgia, 'Times New Roman', Times, serif");
         secondTab.innerHTML = "Visit Us";
         navDiv.appendChild(secondTab);
+        document.querySelectorAll(".nav-div").forEach((value) => {
+            value.setAttribute("style", "margin: 20px; font-family: Segoe UI");
+        });
     }
     const page = () => {
         const content = document.createElement("div");
@@ -54,8 +36,31 @@ const mainPage = () => {
     navBar();
     navContent();
     page();
-    mainContent();
 };
+const mainContent = () => {
+    const contentDiv = document.querySelector("#tab-content");
+    const mainHeading = document.createElement("h1");
+    mainHeading.setAttribute("id", "restaurant-name");
+    mainHeading.setAttribute("style", "text-align: center; font-family: Georgia, 'Times New Roman', Times, serif")
+    mainHeading.innerHTML = "Welcome to Cactus Restaurant and Grill";
+    const descriptionHeading = document.createElement("h2");
+    descriptionHeading.setAttribute("id", "description-heading");
+    descriptionHeading.setAttribute("style", "text-align: center; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif");
+    descriptionHeading.innerHTML = "Our world class flavors combined with our system of an unlimited buffet results in a premium dining experience for our guests."
+    const foodImage = document.createElement("img");
+    foodImage.setAttribute("id", "food");
+    foodImage.setAttribute("src", "../images/food.jpg");
+    foodImage.setAttribute("style", "width: 600px; height: 337.5px; display: block; margin: 10 auto;");
+    const setupImage = document.createElement("img");
+    setupImage.setAttribute("id", "setup");
+    setupImage.setAttribute("src", "../images/setup.jpg");
+    setupImage.setAttribute("style", "width: 600px; height: 337.5px; display: block; margin: auto;");
+    contentDiv.appendChild(mainHeading);
+    contentDiv.appendChild(descriptionHeading);
+    contentDiv.appendChild(foodImage);
+    contentDiv.appendChild(setupImage);
+}
 
-export {mainPage};
+
+export {mainPage, mainContent};
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import {mainPage} from "./home.js"
+import {mainPage, mainContent} from "./home.js"
 import {menuContent} from "./menu.js"
 import {visitContent} from "./visit.js"
 mainPage();
-menuContent();
-visitContent();
+mainContent();
+const tabContent = document.querySelector("#tab-content");
+function resetPage() {
+    tabContent.innerHTML = "";
+}
+function setEventListeners() {
+    const inputList = document.querySelectorAll(".nav-div");
+    inputList[0].addEventListener("click", () => {
+        resetPage();
+        mainContent();
+    });
+    inputList[1].addEventListener("click", () => {
+        resetPage();
+        menuContent();
+    });
+    inputList[2].addEventListener("click", () => {
+        resetPage();
+        visitContent();
+    })
+}
+setEventListeners();
